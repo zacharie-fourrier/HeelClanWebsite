8052703b4d9f16e36a3ae77f71c1dda6845530df
HEELZacky <zacharie.fourrier@gmail.com>
2022-07-26 13:13:59 +0200

    Added Style and Script sheets

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeFaviconByTheme(){
+    const icon = document.getElementById('icon');
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+    const logoWhite = "img/Icon White.svg";
+    const logoBlack = "img/Icon Black.svg";
+    
+    if (isDark.matches) {
+        icon.href=logoWhite;
+    } else {
+        icon.href=logoBlack;
+    }
+}
