f5b5c18c86716af6af285284afab0eb38be22973
HEELZacky <95706083+HEELZacky@users.noreply.github.com>
2022-09-30 16:34:47 +0200

    frontend again

--- Logo.js
+++ Logo.js
@@ -1,14 +1,14 @@
 logoLink();
 
 function logoLink(){
-    var isDark = window.matchMedia("(prefers-color-scheme: dark)");
-    const logoWhite = "/img/Icon White.svg";
+    var isDark = window.matchMedia("(prefers-color-scheme: dark)"); //gets color scheme media
+    const logoWhite = "/img/Icon White.svg"; //set logos path
     const logoBlack = "/img/Icon Black.svg";
 
     if (isDark.matches) {
-        document.getElementById("homeLogo").src = logoWhite;
+        document.getElementById("homeLogo").src = logoWhite; //color scheme is dark so white pic
     } else {
-        document.getElementById("homeLogo").src = logoBlack;
+        document.getElementById("homeLogo").src = logoBlack; //color scheme is light so black pic
     }
     return;
 }
