fc94bfbd7d4117b533bc56f9ae81895bc90b41ea
HEELZacky <95706083+HEELZacky@users.noreply.github.com>
2022-09-30 09:46:15 +0200

    Worked a lot, brother

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 changeFaviconByTheme();
+changeHomeLogoByTheme()
 
 function changeFaviconByTheme(){
     const icon = document.getElementById('icon');
     const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-    const logoWhite = "img/Icon White.svg";
-    const logoBlack = "img/Icon Black.svg";
+    const logoWhite = "/img/Icon White.svg";
+    const logoBlack = "/img/Icon Black.svg";
 
     if (isDark.matches) {
         icon.href=logoWhite;
     } else {
         icon.href=logoBlack;
+    }
+}
+
+function changeHomeLogoByTheme(){
+    const icon = document.getElementById('homeLogo');
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+    const logoWhite = "/img/Icon White.svg";
+    const logoBlack = "/img/Icon Black.svg";
+
+    if (isDark.matches) {
+        icon.src=logoWhite;
+    } else {
+        icon.src=logoBlack;
     }
 }
